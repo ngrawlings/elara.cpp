8d9818467d412f37578e293f9a39b577c29435ea
Nyhl <ngrawlings@gmail.com>
2026-05-02 00:42:38 +0200

    Added the Widget Listener shape.

---
+++ libElaraUI/libelaraui/frontend/listeners/WidgetListener.h
@@ -0,0 +1,63 @@
+#ifndef ELARA_LISTENER_H
+#define ELARA_LISTENER_H
+
+#include "../ElaraWidgetHandle.h"
+
+namespace elara {
+
+class WidgetListener {
+public:
+    virtual ~WidgetListener() {}
+
+    virtual void onWidgetMouseMove(
+        ElaraWidgetHandle handle,
+        double x,
+        double y
+    ) {}
+
+    virtual void onWidgetMouseDown(
+        ElaraWidgetHandle handle,
+        int button,
+        double x,
+        double y
+    ) {}
+
+    virtual void onWidgetMouseUp(
+        ElaraWidgetHandle handle,
+        int button,
+        double x,
+        double y
+    ) {}
+
+    virtual void onWidgetClicked(
+        ElaraWidgetHandle handle,
+        int button,
+        double x,
+        double y
+    ) {}
+
+    virtual void onWidgetHoverChanged(
+        ElaraWidgetHandle handle,
+        bool hovered
+    ) {}
+
+    virtual void onWidgetKeyDown(
+        ElaraWidgetHandle handle,
+        unsigned int keyval
+    ) {}
+
+    virtual void onWidgetKeyUp(
+        ElaraWidgetHandle handle,
+        unsigned int keyval
+    ) {}
+
+    virtual void onWidgetKeysTyped(
+        ElaraWidgetHandle handle,
+        const String& text
+    ) {}
+
+};
+
+}
+
+#endif
